85ae15fdddca7f6e211b3f0811f0c617987a83ea
Allan Carlos <allancarlosdeveloper@gmail.com>
2023-01-19 15:13:34 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Getting Started
+
+<br>
+
+### Gerando o app
+
+<code>flutter create bolso_taller
+--platforms=android</code>
---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,46 @@
+// Create a dashboard finance screen
+// This screen will be the main screen of the app
+// It will contain a list of all the transactions
+// It will also contain a chart of the last 7 days of transactions
+// It will also contain a button to add a new transaction
+
+import 'package:flutter/material.dart';
+// import 'package:bolso_taller/models/transaction.dart';
+// import 'package:bolso_taller/widgets/chart.dart';
+// import 'package:bolso_taller/widgets/new_transaction.dart';
+// import 'package:bolso_taller/widgets/transaction_list.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Expenses'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('tela')
+            // Chart(),
+            // TransactionList(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        // onPressed: () => _startAddNewTransaction(context),
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
